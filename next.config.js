228b028c770786c342705a4fbdc907ecce7cb11d
Viet Cao <viet.ch2612@gmail.com>
2023-11-27 00:19:57 +0700

    Next.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withImages = require('next-images');
 
-module.exports = nextConfig
+const redirects = {
+  async redirects() {
+    return [
+      {
+        source: '/dashboards',
+        destination: '/dashboards/crypto',
+        permanent: true
+      }
+    ];
+  }
+};
+
+module.exports = withImages(redirects);
